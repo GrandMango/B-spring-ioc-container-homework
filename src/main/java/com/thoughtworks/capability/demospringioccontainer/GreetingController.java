1dd9a9771e55410ce893b070b6626240de20ee81
Yuecheng Pei <yuecheng.pei@Yuechengs-MacBook-Pro.local>
2020-06-18 16:35:11 +0800

    p2 -1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,17 @@ public class GreetingController {
 
     @Autowired
     public GreetingController(GreetingService greetingService) {
+        System.out.println("Creating GreetingController.");
         this.greetingService = greetingService;
     }
 
     @GetMapping("/greet")
     public String greet() {
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext();
+//        GreetingService prototypebean1 = (GreetingService) context.getBean("greetingservice");
+//        System.out.println(prototypebean1);
+
         return greetingService.sayHi();
     }
 
